e5c0f094a25aa7fa0ffc6e5195a877e17f29c0c1
Xiao <cangqiongyishao@gmail.com>
2024-08-26 22:49:34 -0500

    add login and frame page

---
+++ src/views/login/login.vue
@@ -0,0 +1,11 @@
+<template>
+    <h1>This is login page</h1>
+</template>
+
+<script setup name="login">
+
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/views/main/frame.vue
@@ -0,0 +1,11 @@
+<template>
+    <h1>This is frame page</h1>
+</template>
+
+<script setup name="frame">
+
+</script>
+
+<style scoped>
+
+</style>
