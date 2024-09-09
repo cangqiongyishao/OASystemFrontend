06da081225c321474093d547a419a9db2b25d5a8
Xiao <cangqiongyishao@gmail.com>
2024-09-08 23:16:59 -0500

    aside initial

--- src/views/main/frame.vue
+++ src/views/main/frame.vue
@@ -3,7 +3,46 @@
 <template>
   <el-container class="container">
     <el-aside class="aside" width="250px">
-        <Router-link to="/" class="brand">XiaoOASystem</Router-link>
+      <Router-link to="/" class="brand">XiaoOASystem</Router-link>
+      <el-menu
+        active-text-color="#ffd04b"
+        background-color="#343a40"
+        class="el-menu-vertical-demo"
+        default-active="2"
+        text-color="#fff"
+        @open="handleOpen"
+        @close="handleClose"
+      >
+        <el-sub-menu index="1">
+          <template #title>
+            <el-icon><location /></el-icon>
+            <span>FrontPage</span>
+          </template>
+          <el-menu-item-group title="Group One">
+            <el-menu-item index="1-1">item one</el-menu-item>
+            <el-menu-item index="1-2">item two</el-menu-item>
+          </el-menu-item-group>
+          <el-menu-item-group title="Group Two">
+            <el-menu-item index="1-3">item three</el-menu-item>
+          </el-menu-item-group>
+          <el-sub-menu index="1-4">
+            <template #title>item four</template>
+            <el-menu-item index="1-4-1">item one</el-menu-item>
+          </el-sub-menu>
+        </el-sub-menu>
+        <el-menu-item index="2">
+          <el-icon><icon-menu /></el-icon>
+          <span>Navigator Two</span>
+        </el-menu-item>
+        <el-menu-item index="3" disabled>
+          <el-icon><document /></el-icon>
+          <span>Navigator Three</span>
+        </el-menu-item>
+        <el-menu-item index="4">
+          <el-icon><setting /></el-icon>
+          <span>Navigator Four</span>
+        </el-menu-item>
+      </el-menu>
     </el-aside>
     <el-container>
       <el-header class="header">Header</el-header>
@@ -13,31 +52,35 @@
 </template>
 
 <style scoped>
-.aside{
-    background-color: #343a40; 
-    shadow:0 14px 28px rgba(0,0,0,.25),0 10px 10px rgba(0,0,0,.22) !important
+.aside {
+  background-color: #343a40;
+  shadow: 0 14px 28px rgba(0, 0, 0, 0.25), 0 10px 10px rgba(0, 0, 0, 0.22) !important;
 }
 
-.container{ 
-    height:100vh;
-    background-color: #f4f6f9;
+.container {
+  height: 100vh;
+  background-color: #f4f6f9;
 }
 
-.aside .brand{
-    color:#fff;
-    text-decoration: none;
-    border-bottom: 1px solid #434a50;
-    background-color: #232631;
-    height: 60px;
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    font-size: 20px;
+.aside .brand {
+  color: #fff;
+  text-decoration: none;
+  border-bottom: 1px solid #434a50;
+  background-color: #232631;
+  height: 60px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  font-size: 20px;
 }
 
-.header{
-    height: 60px;
-    background-color: #fff;
-    border-bottom: 1px solid #e6e6e6;
+.header {
+  height: 60px;
+  background-color: #fff;
+  border-bottom: 1px solid #e6e6e6;
+}
+
+.el-menu{
+  border-right: None;
 }
 </style>
