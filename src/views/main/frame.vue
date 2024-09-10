7892adc4fd93e4be7e2e346c46adb4d1d68aa341
Xiao <cangqiongyishao@gmail.com>
2024-09-09 22:16:24 -0500

    aside menu bar done

--- src/views/main/frame.vue
+++ src/views/main/frame.vue
@@ -8,40 +8,57 @@
         active-text-color="#ffd04b"
         background-color="#343a40"
         class="el-menu-vertical-demo"
-        default-active="2"
+        default-active="1"
         text-color="#fff"
-        @open="handleOpen"
-        @close="handleClose"
       >
-        <el-sub-menu index="1">
+        <el-menu-item index="1">
+          <el-icon><HomeFilled /></el-icon>
+          <span>FrontPage</span>
+        </el-menu-item>
+        <el-sub-menu index="2">
           <template #title>
-            <el-icon><location /></el-icon>
-            <span>FrontPage</span>
+            <el-icon><Checked /></el-icon>
+            <span>Attendance Management</span>
           </template>
-          <el-menu-item-group title="Group One">
-            <el-menu-item index="1-1">item one</el-menu-item>
-            <el-menu-item index="1-2">item two</el-menu-item>
-          </el-menu-item-group>
-          <el-menu-item-group title="Group Two">
-            <el-menu-item index="1-3">item three</el-menu-item>
-          </el-menu-item-group>
-          <el-sub-menu index="1-4">
-            <template #title>item four</template>
-            <el-menu-item index="1-4-1">item one</el-menu-item>
-          </el-sub-menu>
+          <el-menu-item index="2-1">
+            <el-icon><UserFilled /></el-icon>
+            <span>Personal Attendance</span>
+          </el-menu-item>
+          <el-menu-item index="2-2">
+            <el-icon><User /></el-icon>
+            <span>Subordinate Attendance</span>
+          </el-menu-item>
         </el-sub-menu>
-        <el-menu-item index="2">
-          <el-icon><icon-menu /></el-icon>
-          <span>Navigator Two</span>
-        </el-menu-item>
-        <el-menu-item index="3" disabled>
-          <el-icon><document /></el-icon>
-          <span>Navigator Three</span>
-        </el-menu-item>
-        <el-menu-item index="4">
-          <el-icon><setting /></el-icon>
-          <span>Navigator Four</span>
-        </el-menu-item>
+
+        <el-sub-menu index="3">
+          <template #title>
+            <el-icon><BellFilled /></el-icon>
+            <span>Notification Management</span>
+          </template>
+          <el-menu-item index="3-1">
+            <el-icon><CirclePlusFilled /></el-icon>
+            <span>Publish Notification</span>
+          </el-menu-item>
+          <el-menu-item index="3-2">
+            <el-icon><List /></el-icon>
+            <span>Notification List</span>
+          </el-menu-item>
+        </el-sub-menu>
+
+        <el-sub-menu index="4">
+          <template #title>
+            <el-icon><Avatar /></el-icon>
+            <span>Employee Management</span>
+          </template>
+          <el-menu-item index="4-1">
+            <el-icon><CirclePlusFilled /></el-icon>
+            <span>New Employee</span>
+          </el-menu-item>
+          <el-menu-item index="4-2">
+            <el-icon><List /></el-icon>
+            <span>Employee List</span>
+          </el-menu-item>
+        </el-sub-menu>
       </el-menu>
     </el-aside>
     <el-container>
@@ -80,7 +97,7 @@
   border-bottom: 1px solid #e6e6e6;
 }
 
-.el-menu{
+.el-menu {
   border-right: None;
 }
 </style>
