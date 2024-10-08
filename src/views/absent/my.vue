00b3d3497da5e146f8ddad61e0842a4ebeee3e2e
Xiao <cangqiongyishao@gmail.com>
2024-10-07 20:33:15 -0500

    pagination

--- src/views/absent/my.vue
+++ src/views/absent/my.vue
@@ -1,7 +1,7 @@
 <script setup name="myabsent">
 import OAPageHeader from "@/components/OAPageHeader.vue";
 import absentHttp from "@/api/absentHttp";
-import { ref, reactive, onMounted, computed } from "vue";
+import { ref, reactive, onMounted, computed,watch } from "vue";
 import { ElMessage } from "element-plus";
 import timeFormatter from "@/utils/timeFormatter";
 
@@ -37,6 +37,11 @@ let responder = reactive({
   realname: "",
 });
 
+let pagination=reactive({
+    total:0,
+    page:1
+})
+
 let responder_str = computed(() => {
   if (responder.email) {
     return "[" + responder.email + "]" + responder.realname;
@@ -44,6 +49,22 @@ let responder_str = computed(() => {
     return "Null";
   }
 });
+
+watch(()=>pagination.page,(value)=>{
+    requestAbsents(value)
+})
+
+const requestAbsents=async (page)=>{
+    try{
+        let absents_data = await absentHttp.getMyAbsents(page);
+        let total =absents_data.count;
+        pagination.total=total
+        let results=absents_data.results;
+        absents.value = results;
+    }catch(detail){
+        ElMessage.error(detail);
+    }
+}
 
 const OnShowDialog = () => {
   absent_form.title = "";
@@ -82,9 +103,7 @@ onMounted(async () => {
     let responder_data = await absentHttp.getResponder();
     Object.assign(responder, responder_data);
 
-    let absents_data = await absentHttp.getMyAbsents();
-    absents.value = absents_data;
-    console.log(absents.value);
+    requestAbsents(1)
   } catch (detail) {
     ElMessage.error(detail);
   }
@@ -101,39 +120,50 @@ onMounted(async () => {
     </el-card>
     <el-card>
       <el-table :data="absents" style="width: 100%">
-        <el-table-column prop="title" label="Title"  />
-        <el-table-column prop="absent_type.name" label="Style"  />
+        <el-table-column prop="title" label="Title" />
+        <el-table-column prop="absent_type.name" label="Style" />
         <el-table-column prop="request_content" label="Reason" />
-        <el-table-column label="Create Time"> 
-            <template #default="scope">
-                {{ timeFormatter.stringFromDateTime(scope.row.create_time) }}
-            </template>
+        <el-table-column label="Create Time">
+          <template #default="scope">
+            {{ timeFormatter.stringFromDateTime(scope.row.create_time) }}
+          </template>
         </el-table-column>
 
-        <el-table-column label="Start Date"> 
-            <template #default="scope">
-                {{ timeFormatter.stringFromDate(scope.row.start_date) }}
-            </template>
+        <el-table-column label="Start Date">
+          <template #default="scope">
+            {{ timeFormatter.stringFromDate(scope.row.start_date) }}
+          </template>
         </el-table-column>
 
-        <el-table-column label="End Date"> 
-            <template #default="scope">
-                {{ timeFormatter.stringFromDate(scope.row.end_date) }}
-            </template>
+        <el-table-column label="End Date">
+          <template #default="scope">
+            {{ timeFormatter.stringFromDate(scope.row.end_date) }}
+          </template>
         </el-table-column>
 
         <el-table-column label="Responder">
-            {{ responder_str }}
+          {{ responder_str }}
         </el-table-column>
-        <el-table-column prop="response_content" label="Response Content"/>
-        <el-table-column  label="Response Content">
-            <template #default="scope">
-                <el-tag type="info" v-if="scope.row.status==1">Auditing</el-tag>
-                <el-tag type="success" v-else-if="scope.row.status==2">Pass</el-tag>
-                <el-tag type="danger" v-else>Reject</el-tag>
-            </template>
+        <el-table-column prop="response_content" label="Response Content" />
+        <el-table-column label="Response Content">
+          <template #default="scope">
+            <el-tag type="info" v-if="scope.row.status == 1">Auditing</el-tag>
+            <el-tag type="success" v-else-if="scope.row.status == 2"
+              >Pass</el-tag
+            >
+            <el-tag type="danger" v-else>Reject</el-tag>
+          </template>
         </el-table-column>
       </el-table>
+      <template #footer>
+        <el-pagination
+          background
+          layout="prev, pager, next"
+          :page-size="10"
+          :total="pagination.total"
+          v-model:current-page="pagination.page"
+        />
+      </template>
     </el-card>
   </el-space>
 
@@ -198,4 +228,12 @@ onMounted(async () => {
   </el-dialog>
 </template>
 
-<style lang="scss" scoped></style>
+<style  scoped>
+.el-pagination{
+    justify-content: center;
+}
+
+.el-space :deep(.el-space__item)  {
+    width: 100%;
+}
+</style>
